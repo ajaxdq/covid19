47574dd16567cb352ab78c60948ac83a226a47f0
josephexp <joseph.antony@experionglobal.com>
2020-07-16 22:21:11 +0530

    sec5

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/cook-book'));
+app.use(express.static(__dirname + '/dist/CookBook'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/cook-book/index.html'));
+res.sendFile(path.join(__dirname+'/dist/CookBook/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
