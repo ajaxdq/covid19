7872656a2fe00d697acf9f8af87d2a8629518969
josephexp <joseph.antony@experionglobal.com>
2020-08-03 22:56:57 +0530

    sec76

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,9 +9,9 @@ export class AppComponent implements OnInit{
   title = 'Covid19';
 
   ngOnInit(){
-    // setTimeout(() => {
-    //   let splashScreen = document.getElementById('splashScreenClass');
-    //   splashScreen.remove();
-    // }, 6000);
+    setTimeout(() => {
+      let splashScreen = document.getElementById('splashScreenClass');
+      splashScreen.remove();
+    }, 6000);
   }
 }
